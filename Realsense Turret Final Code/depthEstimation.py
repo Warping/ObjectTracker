4403ba52e7ed05873983a157d99e203f73f4251d
mewowz <mewowz@lainx.org>
2024-02-21 14:57:31 -0800

    Added a depth estimation algorithm

---
+++ Realsense Turret Final Code/depthEstimation.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+pixel_heights = []
+depth_points = []
+
+MAX_K_SIZE = 100
+k_final = []
+
+def estimate(pixel_h, depth_c):
+    k = []
+    for h, d in enumerate(pixel_h, depth_c):
+        k.append(h*d)
+    k_avg = np.mean(k)
+    global k_final
+    k_final.append(k_avg)
+    if len(k_final) > 100:
+        k_final.pop(0)
+
+    return 
+
+
+def get_real_depth(pixel_h, depth_c, resolution=0.1):
+    if np.std(depth_c) < resolution:
+        estimate(pixel_h, depth_c)
+        return np.mean(depth_c)
+    elif len(k_final) != 0:
+        depths = []
+        global k_final
+        k = np.mean(k_final)
+        for h in pixel_h:
+            depths.append(k/h)
+        if np.std(depths) < resolution:
+            return np.mean(depths)
+        else:
+            return None
+    else:
+        return None
