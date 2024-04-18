3421c6a748f16441bc0ac1dd85963017f22265c0
mewowz <mewowz@lainx.org>
2024-04-18 12:24:13 -0700

    Fixed both get_cost_matrix and associate_detections to handle all possible cases

--- Realsense Turret Final Code/association/hungarian_association.py
+++ Realsense Turret Final Code/association/hungarian_association.py
@@ -7,17 +7,10 @@ def get_cost_matrix(targets, old_positions, detections):
     if len(targets) != len(old_positions):
         raise ValueError(f'The amount of targets must be equal to the amount of old positions')
 
-    # The cost matrix needs to be a squre matrix, so if there are more workers
-    # than jobs or vice versa, add dummy rows/cols
-    h = 0
-    w = 0
-    if len(targets) < len(detections):
-        h = len(detections)
-        w = h
-    else:
-        h = len(targets)
-        w = h
-    cost_matrix = np.zeros((h,w))
+    high_assignment_cost = 10000 # a cost that we will never reach in the scope of this product
+    rows = len(targets)
+    cols = len(detections)
+    cost_matrix = np.full((rows,cols), fill_value=high_assignment_cost)
     for i, target in enumerate(targets):
         for j, detection in enumerate(detections):
             targ_pos = old_positions[i]
@@ -27,25 +20,40 @@ def get_cost_matrix(targets, old_positions, detections):
 def associate_detections(targets, detections, cost_matrix, return_type='list'):
     row, col = linear_sum_assignment(cost_matrix)
 
-    if return_type == 'dict':
-        associations = {}
+    num_targets = len(targets)
+    num_detections = len(detections)
+    associations = []
+
+    if num_targets > num_detections:
+        for r in range(num_targets):
+            min_cost_index = np.argmin(cost_matrix[r, :num_detections])
+            associations.append((targets[r], detections[min_cost_index]))
+    else:
         for r,c in zip(row,col):
-             associations[targets[r]] = detections[c]
+            if c < num_detections and r < num_targets:
+                associations.append((targets[r],detections[c]))
+
+
+         
+
+    if return_type == 'dict':
+        assoc_dict = {}
+        names = [n[0] for n in associations]
+        match = [n[1] for n in associations]
+        for n,m in zip(names,match):
+            assoc_dict[n] = m
+        return assoc_dict
+    else:
         return associations
 
-    else: #default case, list
-        associations = []
-        for r,c in zip(row, col):
-            associations.append((targets[r],detections[c]))
-        return associations
-    
-def associate(targets, old_positions, detections):
+            
+def associate(targets, old_positions, detections, return_type='list'):
     """
     A wrapper for doing a full Hungarian Algorithm call. `targets` can be anything
     but likely will be the objects that are being referenced and old_positions is likely
     going to be said targets' _ukf.x. If doing association merely on positions
     """
     cost_matrix = get_cost_matrix(targets, old_positions, detections)
-    associations = associate_detections(targets, detections, cost_matrix)
+    associations = associate_detections(targets, detections, cost_matrix, return_type=return_type)
     return associations
 
